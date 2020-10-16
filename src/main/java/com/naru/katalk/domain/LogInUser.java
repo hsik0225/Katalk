5f18682ac4acb7034d1c99ccb042dea194897d6a
naru <hsik0225@gmail.com>
2020-10-17 01:32:52 +0900

    feat : User 클래스를 상속받는 LogInUser 클래스 정의
    LogInUser는 @Entity이며, LogIn 한 유저가 할 수 있는 여러 가지 일을 수행한다

---
+++ src/main/java/com/naru/katalk/domain/LogInUser.java
@@ -0,0 +1,34 @@
+package com.naru.katalk.domain;
+
+public class LogInUser extends User {
+
+    private Profile profile;
+
+    public void changePassword() {
+
+    }
+
+    public void changeUserName() {
+
+    }
+
+    public void changePicture() {
+
+    }
+
+    public void enterRoom() {
+
+    }
+
+    public void exitRoom() {
+
+    }
+
+    public void sendMessage() {
+
+    }
+
+    public void readMessage() {
+
+    }
+}
