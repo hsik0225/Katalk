f2d5044da335c999f820d16ed703891fc126c1fc
naru <hsik0225@gmail.com>
2020-10-16 13:31:36 +0900

    chore : 프로젝트 빌드

---
+++ src/main/java/com/naru/katalk/KatalkApplication.java
@@ -0,0 +1,13 @@
+package com.naru.katalk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KatalkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KatalkApplication.class, args);
+    }
+
+}
