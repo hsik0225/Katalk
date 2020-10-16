46359e127e574d7304abb75985d2e5a7da58e1b8
naru <hsik0225@gmail.com>
2020-10-17 01:32:06 +0900

    feat : 유저의 인증을 위해 필요한 정보가 아닌 그 외에 정보들을 Profile 클래스로 정의

---
+++ src/main/java/com/naru/katalk/domain/Profile.java
@@ -0,0 +1,24 @@
+package com.naru.katalk.domain;
+
+public class Profile {
+
+    private String userName;
+
+    private String picture;
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPicture() {
+        return picture;
+    }
+
+    public void setPicture(String picture) {
+        this.picture = picture;
+    }
+}
