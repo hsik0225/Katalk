395d0ebad2bf748f1e39bbb49432c6bb597296c4
naru <hsik0225@gmail.com>
2020-10-19 22:49:26 +0900

    refactor : 원래 User 클래스의 이름을 Member로 바꿈에 따라 관련 클래스 이름 수정

---
+++ src/main/java/com/naru/katalk/domain/Member.java
@@ -0,0 +1,36 @@
+package com.naru.katalk.domain;
+
+public class Member implements User {
+
+    public void logOut() {
+
+    }
+
+    public void changePassword() {
+
+    }
+
+    public void changeUserName() {
+
+    }
+
+    public void changePicture() {
+
+    }
+
+    public void enterRoom() {
+
+    }
+
+    public void exitRoom() {
+
+    }
+
+    public void sendMessage() {
+
+    }
+
+    public void readMessage() {
+
+    }
+}
---
+++ src/main/java/com/naru/katalk/domain/MemberInformation.java
@@ -0,0 +1,8 @@
+package com.naru.katalk.domain;
+
+public class MemberInformation {
+
+    private LoginInformation loginInfo;
+
+    private PersonalInformation personalInfo;
+}
