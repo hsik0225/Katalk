d6489a0a69b904fd9bdc9e23054d9013cc20c581
naru <hsik0225@gmail.com>
2020-10-20 17:11:15 +0900

    refactor : LoginInformation의 이름을 LoginManager로 변경
    - 정보만 담고있는 Information에서 Login과 관련된 기능을 하는 Manager의 이름으로 변경하여 로그인기능도 담당한다

---
+++ src/main/java/com/naru/katalk/domain/SignManager.java
@@ -0,0 +1,38 @@
+package com.naru.katalk.domain;
+
+import lombok.Getter;
+import org.mindrot.jbcrypt.BCrypt;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+
+@Getter
+public class SignManager {
+
+    @Email
+    private String email;
+
+    // null, "", " " 모두 허용하지 않는다
+    @NotBlank
+
+    // 소문자 영어, 숫자가 1번씩 포함되어야 하며 비밀번호는 대/소문자, 숫자만이 가능하다
+    @Pattern(regexp = "^(?=.*[0-9])(?=.*[a-z])[a-zA-Z0-9]*$")
+    private String password;
+
+    private String confirmPassword;
+
+    public boolean checkConfirmPassword() {
+        return this.password.equals(confirmPassword);
+    }
+
+    public void checkPassword(SignManager signManager) {
+        if (!BCrypt.checkpw(signManager.password, this.password)) {
+            throw new IllegalArgumentException("존재하지 않는 이메일이거나 비밀번호가 일치하지 않습니다!");
+        }
+    }
+
+    public void hashPassword() {
+        this.password = BCrypt.hashpw(this.password, BCrypt.gensalt());
+    }
+}
