b813ee324dc6a3f8de0f8e28410ea269981b3f7a
naru <hsik0225@gmail.com>
2020-10-20 17:12:53 +0900

    feat : Member에 어노테이션 추가 및 속성들 추가

--- src/main/java/com/naru/katalk/domain/Member.java
+++ src/main/java/com/naru/katalk/domain/Member.java
@@ -1,8 +1,32 @@
 package com.naru.katalk.domain;
 
+import lombok.*;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Setter
+@Builder
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
 public class Member {
 
-    private MemberInformation memberInfo;
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @Embedded
+    private MemberManager memberManager;
+
+    public Member(MemberManager memberManager) {
+        this.memberManager = memberManager;
+    }
+
+    public void checkPassword(SignManager signManager) {
+        this.memberManager.getSignManager().checkPassword(signManager);
+    }
 
     public void changePassword() {
 
