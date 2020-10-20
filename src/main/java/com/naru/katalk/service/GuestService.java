2e0f24bdb2758327cb7aac7a9bf9636cfff51a8f
naru <hsik0225@gmail.com>
2020-10-20 17:13:34 +0900

    feat : GuestService 클래스에 로그인 및 회원가입 기능 구현

--- src/main/java/com/naru/katalk/service/GuestService.java
+++ src/main/java/com/naru/katalk/service/GuestService.java
@@ -1,14 +1,52 @@
 package com.naru.katalk.service;
 
-import com.naru.katalk.domain.LoginInformation;
-import com.naru.katalk.domain.MemberInformation;
+import com.naru.katalk.domain.SignManager;
+import com.naru.katalk.domain.Member;
+import com.naru.katalk.domain.MemberManager;
+import com.naru.katalk.exception.DuplicateEmailException;
+import com.naru.katalk.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import javax.persistence.EntityNotFoundException;
+import java.util.Optional;
+
+@RequiredArgsConstructor
+@Service
 public class GuestService {
 
+    private final MemberRepository memberRepository;
 
-    public void login(LoginInformation loginInfo) {
+    public void login(SignManager signManager) {
+        Member member = findMemberByEmail(signManager);
+        member.checkPassword(signManager);
     }
 
-    public void register(MemberInformation memberInfo) {
+    @Transactional
+    public void register(MemberManager memberManager) {
+        Member member = new Member(memberManager);
+        SignManager signManager = member.getMemberManager().getSignManager();
+        checkDuplicateEmail(signManager);
+        signManager.hashPassword();
+        memberRepository.save(member);
+    }
+
+    private Optional<Member> findOptMemberByEmail(SignManager signManager) {
+        return memberRepository
+                .findMemberByMemberManager_SignManager_Email(signManager.getEmail());
+    }
+
+    private Member findMemberByEmail(SignManager signManager) {
+        return findOptMemberByEmail(signManager)
+                .orElseThrow(
+                () -> new EntityNotFoundException("이 이메일로 가입된 아이디가 존재하지 않습니다")
+        );
+    }
+
+    private void checkDuplicateEmail(SignManager signManager) {
+        if (findOptMemberByEmail(signManager).isPresent()) {
+            throw new DuplicateEmailException("중복된 이메일입니다");
+        }
     }
 }
