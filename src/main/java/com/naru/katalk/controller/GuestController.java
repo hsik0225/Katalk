4b0453456e8f7c596018cef99b542222f00bd59a
naru <hsik0225@gmail.com>
2020-10-21 21:37:39 +0900

    refactor : GuestController 응답을 Response 클래스로 수정

--- src/main/java/com/naru/katalk/controller/GuestController.java
+++ src/main/java/com/naru/katalk/controller/GuestController.java
@@ -1,9 +1,13 @@
 package com.naru.katalk.controller;
 
+import com.naru.katalk.common.Response;
+import com.naru.katalk.common.SuccessCode;
 import com.naru.katalk.domain.SignManager;
 import com.naru.katalk.domain.MemberManager;
 import com.naru.katalk.service.GuestService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,13 +21,15 @@ public class GuestController {
     private final GuestService guestService;
 
     @PostMapping(path = "/login")
-    public void logIn(@RequestBody SignManager signManager) {
+    public ResponseEntity<Response> logIn(@RequestBody SignManager signManager) {
         guestService.login(signManager);
+        return new ResponseEntity<>(new Response(SuccessCode.LOGGED_IN), HttpStatus.OK);
     }
 
     @PostMapping
-    public void register(@RequestBody MemberManager memberManager) {
+    public ResponseEntity<Response> register(@RequestBody MemberManager memberManager) {
         guestService.register(memberManager);
+        return new ResponseEntity<>(new Response(SuccessCode.SIGNED_UP), HttpStatus.CREATED);
     }
 }
 
