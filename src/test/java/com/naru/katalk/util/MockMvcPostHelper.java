2ea876a362f1c209b9e2cdcea0873ded25ee0f89
naru <hsik0225@gmail.com>
2020-10-21 20:06:08 +0900

    feat : post 를 테스트할 때 기본적으로 들어가는 속성들을 정의하여 메소드로 구현

---
+++ src/test/java/com/naru/katalk/util/MockMvcPostHelper.java
@@ -0,0 +1,19 @@
+package com.naru.katalk.util;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+
+public class MockMvcPostHelper {
+
+    private MockMvcPostHelper() {}
+
+    public static MockHttpServletRequestBuilder postObject(String urlTemplate, Object obj) throws JsonProcessingException {
+        return post(urlTemplate)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(ObjectToJsonConverter.objectToJson(obj))
+                .characterEncoding("UTF-8");
+    }
+}
