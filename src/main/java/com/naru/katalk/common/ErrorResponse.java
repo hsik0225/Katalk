9e3c663867693258bb2dd85019ead6740b80345f
naru <hsik0225@gmail.com>
2020-10-22 14:12:27 +0900

    refactor : ErrorResponse에 final이 붙을 수 있는 변수들에 모두 final 선언 및 주석 추가

--- src/main/java/com/naru/katalk/common/ErrorResponse.java
+++ src/main/java/com/naru/katalk/common/ErrorResponse.java
@@ -7,54 +7,63 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
-public class ErrorResponse extends Response {
+// 상속하여 새로운 관련 인스턴스를 생성하지 못하도록 설정
+public final class ErrorResponse extends Response {
 
     // FieldError를 생성하지 않은 상태로 ErrorResponse를 생성할 수 없도록 한다
-    private ErrorResponse(ErrorCode code) {
+    private ErrorResponse(final ErrorCode code) {
         super(code);
     }
 
-    // FieldError를 생성하지 않은 상태로 ErrorResponse를 생성할 수 없도록 한다
-    private ErrorResponse(ErrorCode code, Object errors) {
+    // 뒤에 받는 Object를 BindingResult로 받을지 List<FieldError>로 받을지 선택하기 위하여
+    // pviate 생성자로 설정한다
+
+    // 만약 생성자를 그대로 사용한다면 어떤 클래스인지 알기 위하여 instanceof 를 사용해야 한다
+    // ex. BindingResult 클래스가 들어왔을 경우 bindingResult instanceof BindingResult
+    private ErrorResponse(final ErrorCode code, final Object errors) {
         super(code, errors);
     }
 
-    public static ErrorResponse of(ErrorCode code, BindingResult bindingResult) {
+    public static ErrorResponse from(final ErrorCode code) {
+        return new ErrorResponse(code);
+    }
+
+    public static ErrorResponse of(final ErrorCode code, final BindingResult bindingResult) {
         return new ErrorResponse(code, FieldError.of(bindingResult));
     }
 
-    public static ErrorResponse of(ErrorCode code, List<FieldError> fieldErrors) {
+    public static ErrorResponse of(final ErrorCode code, final List<FieldError> fieldErrors) {
         return new ErrorResponse(code, fieldErrors);
     }
 
-    public static ErrorResponse of(MethodArgumentTypeMismatchException e) {
-        String value = e.getValue() == null ? "" : e.getValue().toString();
+    public static ErrorResponse of(final MethodArgumentTypeMismatchException e) {
+        final String value = e.getValue() == null ? "" : e.getValue().toString();
         List<ErrorResponse.FieldError> errors = ErrorResponse.FieldError.of(e.getName(), value, e.getErrorCode());
         return new ErrorResponse(ErrorCode.LOGIN_INPUT_INVALID, errors);
     }
 
-    public static class FieldError {
+    private static class FieldError {
 
-        private String field;
+        private final String field;
 
-        private String value;
+        private final String value;
 
-        private String reason;
+        private final String reason;
 
-        private FieldError(String field, String value, String reason) {
+        private FieldError(final String field, final String value, final String reason) {
             this.field = field;
             this.value = value;
             this.reason = reason;
         }
 
-        public static List<FieldError> of(String field, String value, String reason) {
+        public static List<FieldError> of(final String field, final String value, final String reason) {
             List<FieldError> fieldErrors = new ArrayList<>();
             fieldErrors.add(new FieldError(field, value, reason));
             return fieldErrors;
         }
 
-        private static List<FieldError> of(BindingResult bindingResult) {
-            List<org.springframework.validation.FieldError> fieldErrors = bindingResult.getFieldErrors();
+        private static List<FieldError> of(final BindingResult bindingResult) {
+            final List<org.springframework.validation.FieldError> fieldErrors = bindingResult.getFieldErrors();
             return fieldErrors
                     .stream()
                     .map(fieldError -> new FieldError(
