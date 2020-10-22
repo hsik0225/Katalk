ad2cc66d0ccbaad7818dd776fa94b4a7c85a5f35
naru <hsik0225@gmail.com>
2020-10-22 14:07:35 +0900

    refactor : GuestService의 final이 붙을 수 있는 변수들에 모두 final 부여

--- src/main/java/com/naru/katalk/service/GuestService.java
+++ src/main/java/com/naru/katalk/service/GuestService.java
@@ -18,30 +18,30 @@ public class GuestService {
 
     private final MemberRepository memberRepository;
 
-    public void login(SignManager signManager) {
+    public void login(final SignManager signManager) {
         Member member = findMemberByEmail(signManager);
         member.checkPassword(signManager);
     }
 
     @Transactional
-    public void register(MemberManager memberManager) {
+    public void register(final MemberManager memberManager) {
         Member member = new Member(memberManager);
-        SignManager signManager = member.getMemberManager().getSignManager();
+        final SignManager signManager = member.getMemberManager().getSignManager();
         checkDuplicateEmail(signManager);
         signManager.hashPassword();
         memberRepository.save(member);
     }
 
-    private Optional<Member> findOptMemberByEmail(SignManager signManager) {
+    private Optional<Member> findOptMemberByEmail(final SignManager signManager) {
         return memberRepository
                 .findMemberByMemberManager_SignManager_Email(signManager.getEmail());
     }
 
-    private Member findMemberByEmail(SignManager signManager) {
+    private Member findMemberByEmail(final SignManager signManager) {
         return findOptMemberByEmail(signManager).orElseThrow(LoginException::new);
     }
 
-    private void checkDuplicateEmail(SignManager signManager) {
+    private void checkDuplicateEmail(final SignManager signManager) {
         if (findOptMemberByEmail(signManager).isPresent()) {
             throw new DuplicateEmailException("해당 이메일로 가입된 아이디가 존재합니다");
         }
