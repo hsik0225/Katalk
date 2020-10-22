14ed9726f9af5a1d31efce8e3326c1c548a3886c
naru <hsik0225@gmail.com>
2020-10-22 14:06:40 +0900

    refactor : GuestController에서 LoginController로 이름 변경
    - TestCode를 짜다보니 Login과 회원가입을 구분할 필요성을 느꼈다

---
+++ src/test/java/com/naru/katalk/controller/LoginControllerTest.java
@@ -0,0 +1,86 @@
+package com.naru.katalk.controller;
+
+import com.naru.katalk.config.ResultHandlerConfiguration;
+import com.naru.katalk.domain.SignManager;
+import com.naru.katalk.exception.LoginException;
+import com.naru.katalk.exception.advice.UserControllerAdvice;
+import com.naru.katalk.service.GuestService;
+import com.naru.katalk.util.MockMvcPostHelper;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Import;
+import org.springframework.restdocs.RestDocumentationExtension;
+import org.springframework.restdocs.mockmvc.RestDocumentationResultHandler;
+import org.springframework.restdocs.payload.JsonFieldType;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static com.naru.katalk.util.TestAttribute.EMAIL;
+import static com.naru.katalk.util.TestAttribute.PASSWORD;
+import static org.mockito.Mockito.doThrow;
+import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
+import static org.springframework.restdocs.payload.PayloadDocumentation.requestFields;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@ExtendWith({RestDocumentationExtension.class, SpringExtension.class})
+
+/*
+MockMvc 를 주입받는 2가지 방법
+1. @SpringBootTest + @AutoConfigureMockMvc
+2. @WebMvcTest
+ */
+@WebMvcTest(controllers = {LoginController.class, UserControllerAdvice.class})
+
+@Import(ResultHandlerConfiguration.class)
+
+// 디렉토리 경로를 인수로 받아 결과 디렉토리(outputDir)로 설정한다
+// build.gradle 의 설정을 디폴트로 갖는다
+@AutoConfigureRestDocs
+public class LoginControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired UserControllerAdvice userControllerAdvice;
+
+    @Autowired
+    private RestDocumentationResultHandler restDocumentation;
+
+    @MockBean
+    private GuestService guestService;
+
+    private static SignManager signManager;
+
+    @BeforeAll
+    public static void setUp() {
+        signManager = SignManager
+                .builder()
+                .email(EMAIL.getAttribute())
+                .password(PASSWORD.getAttribute())
+                .build();
+    }
+
+    @Test
+    public void 로그인() throws Exception {
+
+        this.mockMvc
+                .perform(MockMvcPostHelper.postObject("/users/login", signManager))
+                .andExpect(status().isOk())
+                .andDo(print())
+                .andDo(restDocumentation.document(
+                        requestFields(
+                                fieldWithPath("email").type(JsonFieldType.STRING).description("The user's email address"),
+                                fieldWithPath("password").type(JsonFieldType.STRING).description("The user's password"),
+                                fieldWithPath("confirmPassword").type(JsonFieldType.STRING).ignored()
+                        )
+                ));
+    }
+}
+
