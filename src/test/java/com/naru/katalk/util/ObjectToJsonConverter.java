c52b5954ac1262d12dc9ee5e85d8238091aa8cfa
naru <hsik0225@gmail.com>
2020-10-22 16:43:48 +0900

    refactor : ObjectMapper INDENT_OUTPUT 설정 삭제
    - Request는 정리해서 출력하게 해주지만 Response에는 적용이 되지 않아 삭제

--- src/test/java/com/naru/katalk/util/ObjectToJsonConverter.java
+++ src/test/java/com/naru/katalk/util/ObjectToJsonConverter.java
@@ -2,16 +2,10 @@ package com.naru.katalk.util;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.SerializationFeature;
 
 public class ObjectToJsonConverter {
 
     private static final ObjectMapper OBJECT_MAPPER = new ObjectMapper();
-
-    // Request & Response 의 JSON 형식을 깔끔하게 정리된 형태로 출력한다
-    static {
-        OBJECT_MAPPER.configure(SerializationFeature.INDENT_OUTPUT, true);
-    }
 
     private ObjectToJsonConverter() {
     }
