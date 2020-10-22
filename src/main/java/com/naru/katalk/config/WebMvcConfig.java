05deef8aea7252b1742ee2c335b097f2f02dda69
naru <hsik0225@gmail.com>
2020-10-22 23:07:22 +0900

    feat : 요청 및 응답 인코딩을 UTF-8로 설정해주는 CharacterEncodingFilter 빈 등록

--- src/main/java/com/naru/katalk/config/WebMvcConfig.java
+++ src/main/java/com/naru/katalk/config/WebMvcConfig.java
@@ -1,6 +1,10 @@
 package com.naru.katalk.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.web.filter.CharacterEncodingFilter;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -12,5 +16,16 @@ public class WebMvcConfig implements WebMvcConfigurer {
         registry
                 .addResourceHandler("/docs/**")
                 .addResourceLocations("classpath:/static/docs/");
+    }
+
+    @Bean
+
+    // 빈 간의 우선순위를 조정한다. HIGHEST_PRECEDENCE는 최우선 순위를 의미한다
+    @Order(Ordered.HIGHEST_PRECEDENCE)
+    public CharacterEncodingFilter characterEncodingFilter() {
+        CharacterEncodingFilter filter = new CharacterEncodingFilter();
+        filter.setEncoding("UTF-8");
+        filter.setForceEncoding(true);
+        return filter;
     }
 }
