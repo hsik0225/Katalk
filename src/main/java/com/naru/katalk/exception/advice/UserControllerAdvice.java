70ee975bf3257c89915be59ea4b14e0b0cf03822
naru <hsik0225@gmail.com>
2020-10-22 14:11:55 +0900

    feat : User의 에러 처리를 담당하는 ControllerAdvice 구현

---
+++ src/main/java/com/naru/katalk/exception/advice/UserControllerAdvice.java
@@ -0,0 +1,24 @@
+package com.naru.katalk.exception.advice;
+
+import com.naru.katalk.common.ErrorCode;
+import com.naru.katalk.common.ErrorResponse;
+import com.naru.katalk.exception.LoginException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@Slf4j
+@RestControllerAdvice
+public class UserControllerAdvice {
+
+    @ExceptionHandler(LoginException.class)
+    protected ResponseEntity<ErrorResponse> handleLoginException(final LoginException e) {
+        return handleExceptionClass(LoginException.class, ErrorResponse.from(ErrorCode.LOGIN_INPUT_INVALID));
+    }
+
+    private static ResponseEntity<ErrorResponse> handleExceptionClass(final Class<? extends Throwable> exception, final ErrorResponse errorResponse) {
+        log.error("handle" + exception.getName(), exception);
+        return new ResponseEntity<>(errorResponse, errorResponse.getHttpStatus());
+    }
+}
