bfeaa06a1985578dba3473199c69963df9925119
naru <hsik0225@gmail.com>
2020-10-23 18:56:30 +0900

    feat : FieldDescriptor를 쉽게 추가하는 FieldDescriptorHelper  클래스 생성

---
+++ src/test/java/com/naru/katalk/util/FieldDescriptorHelper.java
@@ -0,0 +1,34 @@
+package com.naru.katalk.util;
+
+import org.springframework.restdocs.constraints.ConstraintDescriptions;
+import org.springframework.restdocs.payload.FieldDescriptor;
+import org.springframework.restdocs.payload.JsonFieldType;
+
+import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
+import static org.springframework.restdocs.snippet.Attributes.key;
+import static org.springframework.util.StringUtils.collectionToDelimitedString;
+
+public class FieldDescriptorHelper {
+
+    public static FieldDescriptor getFieldDescriptor(String path, String description, boolean isOptional) {
+        FieldDescriptor fieldDescriptor = fieldWithPath(path).type(JsonFieldType.STRING)
+                .description(description)
+                .attributes(key("constraint").value(""));
+
+        return isOptional ? fieldDescriptor.optional() : fieldDescriptor;
+    }
+
+    public static <T> FieldDescriptor getStringFieldDescriptor(String path, String description,
+                                                                Class<T> clazz) {
+        String property = path.contains("\\.") ? path.split("\\.")[1] : path;
+        ConstraintDescriptions descriptions = new ConstraintDescriptions(clazz);
+
+        // collectionToDelimitedString : 콜렉션을 delimiter 로 연결한다
+        String constraintMessages =
+                collectionToDelimitedString(descriptions.descriptionsForProperty(property), ".");
+
+        return fieldWithPath(path).type(JsonFieldType.STRING)
+                .description(description)
+                .attributes(key("constraint").value(constraintMessages));
+    }
+}
