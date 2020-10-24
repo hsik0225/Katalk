fa21b0e85e9a171d4f7f17c61f5d49396493e1b5
naru <hsik0225@gmail.com>
2020-10-24 17:52:03 +0900

    feat : chatName에 constraint 추가

--- src/main/java/com/naru/katalk/domain/ProfileManager.java
+++ src/main/java/com/naru/katalk/domain/ProfileManager.java
@@ -1,6 +1,8 @@
 package com.naru.katalk.domain;
 
 import javax.persistence.Embeddable;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -20,6 +22,8 @@ public class ProfileManager {
 
     private static final String PICTURE = "s3.com";
 
+    @Size(min = 1, max = 16)
+    @NotBlank
     // 대화명
     private String chatName;
 
