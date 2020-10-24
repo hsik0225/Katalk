e97c2a40abb6f58d0ed2699e0dd82965eb432bc8
naru <hsik0225@gmail.com>
2020-10-24 15:59:10 +0900

    feat : RestDocs의 요청에 host, content-length가 나오지 않게 수정 및 응답에 content-length가 나오지 않게 수정

--- src/test/java/com/naru/katalk/config/ResultHandlerConfiguration.java
+++ src/test/java/com/naru/katalk/config/ResultHandlerConfiguration.java
@@ -8,6 +8,7 @@ import org.springframework.restdocs.mockmvc.RestDocumentationResultHandler;
 import static org.springframework.restdocs.operation.preprocess.Preprocessors.preprocessRequest;
 import static org.springframework.restdocs.operation.preprocess.Preprocessors.preprocessResponse;
 import static org.springframework.restdocs.operation.preprocess.Preprocessors.prettyPrint;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.removeHeaders;
 
 // @TestConfiguration을 @import 어노테이션을 사용하여 @TestConfiguration을 호출하는 클래스에서만 적용된다
 // @Configuration은 모든 클래스에 적용된다
@@ -19,7 +20,14 @@ public class ResultHandlerConfiguration {
         // RestDocs 스니펫 이름 설정 및 Request 와 Response 를 정리하여 출력
         return MockMvcRestDocumentation
                 .document("{ClassName}/{methodName}",
-                        preprocessRequest(prettyPrint()),
-                        preprocessResponse(prettyPrint()));
+                        preprocessRequest(
+                                prettyPrint(),
+                                removeHeaders("Host", "Content-Length")
+                        ),
+                        preprocessResponse(
+                                prettyPrint(),
+                                removeHeaders("Content-Length")
+                        )
+                );
     }
 }
