b806f462ea3eff138b0e56e4aa732577151cc539
naru <hsik0225@gmail.com>
2020-10-25 03:36:29 +0900

    refactor : REST Docs 필드 제약 조건 설명 앞에 한글 설명 추가할 수 있는 기능 구현

--- src/main/java/com/naru/katalk/domain/SignManager.java
+++ src/main/java/com/naru/katalk/domain/SignManager.java
@@ -32,8 +32,8 @@ public class SignManager {
     @Email
     private String email;
 
-    // 소문자 영어, 숫자가 1번씩 포함되어야 하며 비밀번호는 대/소문자, 숫자만이 가능하다
-    @Pattern(regexp = "^(?=.*[0-9])(?=.*[a-z])[a-zA-Z0-9]*$", message = "소문자 영어, 숫자가 1번씩 포함되어야 하며 비밀번호는 대/소문자, 숫자만이 가능하다")
+    // 소문자 영어, 숫자가 1번씩 포함되어야 하며 비밀번호는 대/소문자, 숫자만이 가능합니다
+    @Pattern(regexp = "^(?=.*[0-9])(?=.*[a-z])[a-zA-Z0-9]*$", message = "소문자 영어, 숫자가 1번씩 포함되어야 하며 비밀번호는 대/소문자, 숫자만이 가능합니다.")
     @Size(min = 6, max = 16)
     private String password;
 
--- src/test/java/com/naru/katalk/util/FieldDescriptorHelper.java
+++ src/test/java/com/naru/katalk/util/FieldDescriptorHelper.java
@@ -1,6 +1,12 @@
 package com.naru.katalk.util;
 
-import org.springframework.restdocs.constraints.ConstraintDescriptions;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.restdocs.constraints.Constraint;
+import org.springframework.restdocs.constraints.ConstraintDescriptionResolver;
+import org.springframework.restdocs.constraints.ResourceBundleConstraintDescriptionResolver;
+import org.springframework.restdocs.constraints.ValidatorConstraintResolver;
 import org.springframework.restdocs.payload.FieldDescriptor;
 import org.springframework.restdocs.payload.JsonFieldType;
 
@@ -9,6 +15,15 @@ import static org.springframework.restdocs.snippet.Attributes.key;
 import static org.springframework.util.StringUtils.collectionToDelimitedString;
 
 public class FieldDescriptorHelper {
+
+    private static final ValidatorConstraintResolver constraintResolver
+            = new ValidatorConstraintResolver();
+
+    private static final ConstraintDescriptionResolver descriptionResolver =
+            new ResourceBundleConstraintDescriptionResolver();
+
+    private static final List<String> descriptions = new ArrayList<>();
+
 
     public static FieldDescriptor getDescriptor(String path, String description) {
         return getDescriptor(path, description, "");
@@ -26,12 +41,31 @@ public class FieldDescriptorHelper {
 
         String[] properties = path.split("\\.");
         String property = properties[properties.length - 1];
+        String constraintMessages = "";
 
-        ConstraintDescriptions descriptions = new ConstraintDescriptions(clazz);
+        List<Constraint> constraints
+                = constraintResolver.resolveForProperty(property, clazz);
 
-        // collectionToDelimitedString : 컬렉션을 delimiter 로 연결한다
-        String constraintMessages =
-                collectionToDelimitedString(descriptions.descriptionsForProperty(property), ". ");
+        // 제약 조건을 가지고 있지 않을 수 있으므로 if 문으로 한 번 체크합니다
+        if (!constraints.isEmpty()) {
+
+            for (Constraint constraint : constraints) {
+                String message = constraint.getConfiguration().get("message") + " ";
+
+                // 한글이 한 글자도 포함되어 있지 않으면 기본 메세지로 간주하고 추가하지 않습니다
+                if (!message.matches("^.*[가-힣]+.*$")) {
+                    message = "";
+                }
+
+                descriptions.add(message + descriptionResolver.resolveDescription(constraint));
+            }
+
+            // collectionToDelimitedString : 컬렉션을 delimiter 로 연결합니다
+            constraintMessages = collectionToDelimitedString(descriptions, ". ");
+
+            // 저장한 값 삭제
+            descriptions.clear();
+        }
 
         return getDescriptor(path, description, constraintMessages);
     }
