863200eb99ff1df2bb3e8fc782be522ce1bb6491
naru <hsik0225@gmail.com>
2020-10-24 17:51:47 +0900

    feat: /docs에 접속하면 /static/docs/index.html로 가도록 설정

--- src/main/java/com/naru/katalk/config/WebMvcConfig.java
+++ src/main/java/com/naru/katalk/config/WebMvcConfig.java
@@ -5,17 +5,16 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
 import org.springframework.web.filter.CharacterEncodingFilter;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
 
     @Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry
-                .addResourceHandler("/docs/**")
-                .addResourceLocations("classpath:/static/docs/");
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/docs")
+                .setViewName("forward:/docs/index.html");
     }
 
     @Bean
