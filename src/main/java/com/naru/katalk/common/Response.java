7a5cec90293475d5def9981615b422207676e0f4
naru <hsik0225@gmail.com>
2020-10-25 14:49:56 +0900

    feat : 모든 리스폰스의 조상 클래스인 Response 클래스 정의
    - 추상클래스로 설정하여 이 클래스만을 호출하지 못하게 설정. 반드시 상속받은 클래스를 사용하여 안의 속성들을 정의해주어야 한다

--- src/main/java/com/naru/katalk/common/Response.java
+++ src/main/java/com/naru/katalk/common/Response.java
@@ -1,40 +1,25 @@
 package com.naru.katalk.common;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonInclude;
-import com.fasterxml.jackson.annotation.JsonProperty;
 
 import org.springframework.http.HttpStatus;
 
-import lombok.Getter;
-
-@Getter
-
 // NULL 값은 데이터 바인딩하지 않는다
 @JsonInclude(JsonInclude.Include.NON_NULL)
-public class Response {
+public abstract class Response {
 
-    // httpStatus를 데이터 매핑에서 제외
-    @JsonIgnore
-    protected final HttpStatus httpStatus;
+    protected final int statusCode;
+
+    protected final String statusText;
 
     protected final String message;
 
-    // 여기에선 httpStatus와 구분하기 위하여 statusCodeAndText라는 변수명을 사용하지만
-    // 응답에선 status가 더 명확하므로 JsonPropert로 `status`로 설정
-    @JsonProperty(value = "status")
-    protected final String statusCodeAndText;
+    protected final Object body;
 
-    protected Object body;
-
-    public Response(final ResponseStatusCode code) {
-        this.message = code.getMessage();
-        this.httpStatus = code.getHttpStatus();
-        this.statusCodeAndText = this.httpStatus.value() + " " + this.httpStatus.getReasonPhrase();
-    }
-
-    public Response(final ResponseStatusCode code, final Object body) {
-        this(code);
+    public Response(HttpStatus httpStatus, String message, Object body) {
+        this.statusCode = httpStatus.value();
+        this.statusText = httpStatus.getReasonPhrase();
+        this.message = message;
         this.body = body;
     }
 }
