fe805d267e4ae0428da72fdfb05e7a9e1e71a31b
naru <hsik0225@gmail.com>
2020-10-25 14:41:32 +0900

    refactor : member 클래스의 Id 의 이름을 memberId로 변경
    sql에서 확인할 때 속성이 id로만 되어 있으면 외래키로 사용되었을 때 id가 어떤 클래스의 id인지 알 수 없다. 그러므로 어느 클래스의 id인지 알 수 있게 앞에 클래스를 붙임

--- src/main/java/com/naru/katalk/domain/Member.java
+++ src/main/java/com/naru/katalk/domain/Member.java
@@ -24,7 +24,7 @@ public class Member {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    private Long memberId;
 
     @Embedded
     private MemberManager memberManager;
