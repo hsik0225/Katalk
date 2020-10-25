01a154c4a851ba8b22e11bd8f0242ef75674669d
naru <hsik0225@gmail.com>
2020-10-25 20:31:23 +0900

    feat : ControllerAdvice에 ConstraintViolationException에 대한 예외처리 기능 추가

--- src/main/java/com/naru/katalk/exception/advice/UserControllerAdvice.java
+++ src/main/java/com/naru/katalk/exception/advice/UserControllerAdvice.java
@@ -1,5 +1,7 @@
 package com.naru.katalk.exception.advice;
 
+import org.hibernate.exception.ConstraintViolationException;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -9,15 +11,29 @@ import lombok.extern.slf4j.Slf4j;
 import com.naru.katalk.common.ErrorCode;
 import com.naru.katalk.common.ErrorResponse;
 import com.naru.katalk.exception.LoginException;
+import com.naru.katalk.exception.RegisterException;
 
 @Slf4j
 @RestControllerAdvice
 public class UserControllerAdvice {
 
     @ExceptionHandler(LoginException.class)
-    protected ResponseEntity<ErrorResponse> handleLoginException(final LoginException e) {
+    protected ResponseEntity<ErrorResponse> handleLoginException() {
         return handleExceptionClass(LoginException.class,
                 ErrorResponse.from(ErrorCode.LOGIN_INPUT_INVALID));
+    }
+
+    @ExceptionHandler(ConstraintViolationException.class)
+    protected ResponseEntity<ErrorResponse> handleConstraintViolationException(
+            ConstraintViolationException e) {
+        return new ResponseEntity<>(ErrorResponse.of(ErrorCode.EMAIL_DUPLICATION, e),
+                HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(RegisterException.class)
+    protected ResponseEntity<ErrorResponse> handleRegisterException() {
+        return handleExceptionClass(RegisterException.class,
+                ErrorResponse.from(ErrorCode.EMAIL_DUPLICATION));
     }
 
     private static ResponseEntity<ErrorResponse> handleExceptionClass(
