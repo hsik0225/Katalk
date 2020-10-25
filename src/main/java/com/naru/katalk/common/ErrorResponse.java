99b4d0127b4a8e009f67dec58a9625350e527e46
naru <hsik0225@gmail.com>
2020-10-25 20:35:50 +0900

    feat : ErrorResponse가 ConstraintViolation Exception도 처리할 수 있도록 수정

--- src/main/java/com/naru/katalk/common/ErrorResponse.java
+++ src/main/java/com/naru/katalk/common/ErrorResponse.java
@@ -1,23 +1,25 @@
 package com.naru.katalk.common;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
+import org.hibernate.exception.ConstraintViolationException;
+import org.springframework.util.StringUtils;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
+
+import lombok.Getter;
 
 // 상속하여 새로운 관련 인스턴스를 생성하지 못하도록 설정
 public final class ErrorResponse extends Response {
 
     // 생성자를 private을 만들어 불변 객체로 만든다
     private ErrorResponse(final ErrorCode code) {
-        super(code);
+        super(code, null);
     }
 
     // 만약 생성자를 그대로 사용한다면 어떤 클래스인지 알기 위하여 instanceof 를 사용해야 한다
     // ex. BindingResult 클래스가 들어왔을 경우 bindingResult instanceof BindingResult
-    private ErrorResponse(final ErrorCode code, final Object errors) {
+    private ErrorResponse(final ErrorCode code, final FieldError... errors) {
         super(code, errors);
     }
 
@@ -25,22 +27,18 @@ public final class ErrorResponse extends Response {
         return new ErrorResponse(code);
     }
 
-    public static ErrorResponse of(final ErrorCode code, final BindingResult bindingResult) {
-        return new ErrorResponse(code, FieldError.of(bindingResult));
-    }
-
-    public static ErrorResponse of(final ErrorCode code, final List<FieldError> fieldErrors) {
-        return new ErrorResponse(code, fieldErrors);
+    public static ErrorResponse of(final ErrorCode code, final ConstraintViolationException e) {
+        return new ErrorResponse(code, FieldError.from(e, code));
     }
 
     public static ErrorResponse of(final MethodArgumentTypeMismatchException e) {
         final String value = e.getValue() == null ? "" : e.getValue().toString();
-        List<ErrorResponse.FieldError> errors =
-                ErrorResponse.FieldError.of(e.getName(), value, e.getErrorCode());
-        return new ErrorResponse(ErrorCode.LOGIN_INPUT_INVALID, errors);
+        FieldError error = ErrorResponse.FieldError.of(e.getName(), value, e.getErrorCode());
+        return new ErrorResponse(ErrorCode.LOGIN_INPUT_INVALID, error);
     }
 
-    private static class FieldError {
+    @Getter
+    public static class FieldError {
 
         private final String field;
 
@@ -54,16 +52,16 @@ public final class ErrorResponse extends Response {
             this.reason = reason;
         }
 
-        public static List<FieldError> of(final String field, final String value,
-                                          final String reason) {
-            List<FieldError> fieldErrors = new ArrayList<>();
-            fieldErrors.add(new FieldError(field, value, reason));
-            return fieldErrors;
+        public static FieldError of(final String field, final String value,
+                                    final String reason) {
+
+            return new FieldError(field, value, reason);
         }
 
-        private static List<FieldError> of(final BindingResult bindingResult) {
+        private static FieldError[] from(final BindingResult bindingResult) {
             final List<org.springframework.validation.FieldError> fieldErrors =
                     bindingResult.getFieldErrors();
+
             return fieldErrors
                     .stream()
                     .map(fieldError -> new FieldError(
@@ -71,7 +69,19 @@ public final class ErrorResponse extends Response {
                             fieldError.getRejectedValue() == null ? "" :
                                     fieldError.getRejectedValue().toString(),
                             fieldError.getDefaultMessage()))
-                    .collect(Collectors.toList());
+                    .toArray(FieldError[]::new);
+        }
+
+        public static FieldError from(final ConstraintViolationException e, final ErrorCode code) {
+            final String[] fieldAndValue = StringUtils
+                    .trimAllWhitespace(e.getSQLException().getMessage())
+                    .split("'");
+
+            final String field = fieldAndValue[3];
+            final String value = fieldAndValue[1];
+            final String reason = code.getReason();
+
+            return new FieldError(field, value, reason);
         }
     }
 }
