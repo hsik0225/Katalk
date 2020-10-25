be4c910c807f590f250c5fdff633afa7c82101bf
naru <hsik0225@gmail.com>
2020-10-25 22:05:14 +0900

    feat : ErrorCode에 글로벌한 예외처리를 담당하는 메세지 추가

--- src/main/java/com/naru/katalk/common/ErrorCode.java
+++ src/main/java/com/naru/katalk/common/ErrorCode.java
@@ -7,6 +7,9 @@ import lombok.Getter;
 @Getter
 public enum ErrorCode implements ResponseStatusCode {
 
+    // GLOBAL
+    INVALID_INPUT_VALUE(HttpStatus.BAD_REQUEST, "Invalid Input Value", null),
+
     // Member
     EMAIL_DUPLICATION(HttpStatus.CONFLICT, "회원가입 실패", "해당 이메일로 가입된 아이디가 이미 존재합니다"),
     LOGIN_INPUT_INVALID(HttpStatus.UNAUTHORIZED, "로그인 실패", "존재하지 않는 이메일이거나 비밀번호가 일치하지 않습니다");
