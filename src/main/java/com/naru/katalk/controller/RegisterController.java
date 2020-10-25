0e9c2e683c156590aca12de4962f8bc3d68f2a4c
naru <hsik0225@gmail.com>
2020-10-25 22:00:51 +0900

    refactor : RegisterControlle의 @ExceptionHandler 제거
    - 해당 ExceptionHandler로는 예외처리를 잘 처리하기가 어려워서 삭제 후 커스텀 벨리데이터를 구현할 예정

--- src/main/java/com/naru/katalk/controller/RegisterController.java
+++ src/main/java/com/naru/katalk/controller/RegisterController.java
@@ -1,9 +1,7 @@
 package com.naru.katalk.controller;
 
-import org.hibernate.exception.ConstraintViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,7 +11,6 @@ import lombok.RequiredArgsConstructor;
 import com.naru.katalk.common.SuccessCode;
 import com.naru.katalk.common.SuccessResponse;
 import com.naru.katalk.domain.Member;
-import com.naru.katalk.exception.RegisterException;
 import com.naru.katalk.service.GuestService;
 
 @RequiredArgsConstructor
@@ -28,10 +25,5 @@ public class RegisterController {
         guestService.register(member);
         return new ResponseEntity<>(SuccessResponse.from(SuccessCode.SIGNED_UP),
                 HttpStatus.CREATED);
-    }
-
-    @ExceptionHandler(ConstraintViolationException.class)
-    public RegisterException handleConstraintViolationException(ConstraintViolationException e) {
-        return new RegisterException(e);
     }
 }
