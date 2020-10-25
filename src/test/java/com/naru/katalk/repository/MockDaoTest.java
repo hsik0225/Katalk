a78d7dfd4a2502ee0a1497c4c2659d96df4b0ee2
naru <hsik0225@gmail.com>
2020-10-26 02:35:29 +0900

    feat : Repository단을 테스트할 때 공통적으로 사용되는 어노테이션들을 모은 MockDaoTest 클래스 정의

---
+++ src/test/java/com/naru/katalk/repository/MockDaoTest.java
@@ -0,0 +1,15 @@
+package com.naru.katalk.repository;
+
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+/*
+@DataJpaTest 하는 일
+- configuring H2, an in-memory database
+- setting Hibernate, Spring Data, and the DataSource
+- performing an @EntityScan
+- turning on SQL logging
+ */
+@DataJpaTest
+public class MockDaoTest {
+
+}
