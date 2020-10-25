c45a3a3d74c3e225c3ea84e8dde73b1f63a7fdc9
naru <hsik0225@gmail.com>
2020-10-26 02:50:40 +0900

    refactor : ServiceTest를 어노테이션에서 추상 클래스로 변경
    ServiceTest 클래스는 속성도 같이 갖고 있어야 하기 때문에 어노테이션으로는 안된다고 판단

--- src/test/java/com/naru/katalk/service/LoginServiceTest.java
+++ src/test/java/com/naru/katalk/service/LoginServiceTest.java
@@ -2,20 +2,14 @@ package com.naru.katalk.service;
 
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.restdocs.mockmvc.RestDocumentationResultHandler;
 
 import com.naru.katalk.repository.MemberRepository;
 
-@ServiceTest
-public class LoginServiceTest {
+public class LoginServiceTest extends ServiceTest {
 
     @Mock
     private MemberRepository memberRepository;
-
-    @Autowired
-    private RestDocumentationResultHandler restDocumentation;
-
+    
     @Test
     public void loginTest() {
 
--- src/test/java/com/naru/katalk/service/ServiceTest.java
+++ src/test/java/com/naru/katalk/service/ServiceTest.java
@@ -1,25 +1,21 @@
 package com.naru.katalk.service;
 
-import java.lang.annotation.ElementType;
-import java.lang.annotation.Inherited;
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-import java.lang.annotation.Target;
-
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
 import org.springframework.restdocs.RestDocumentationExtension;
+import org.springframework.restdocs.mockmvc.RestDocumentationResultHandler;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import com.naru.katalk.config.ResultHandlerConfiguration;
 
-@Target(ElementType.TYPE)
-@Retention(RetentionPolicy.RUNTIME)
-@Inherited
 @ExtendWith({RestDocumentationExtension.class, SpringExtension.class, MockitoExtension.class})
 @AutoConfigureRestDocs
 @ContextConfiguration(classes = ResultHandlerConfiguration.class)
-public @interface ServiceTest {
+public abstract class ServiceTest {
+
+    @Autowired
+    private RestDocumentationResultHandler restDocumentation;
 }
