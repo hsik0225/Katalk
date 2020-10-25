fadfd681c143a99ce379ae81f4ce445444e2135e
naru <hsik0225@gmail.com>
2020-10-25 22:04:32 +0900

    refactor : 사용하지 않는 exception 일단 삭제

--- src/main/java/com/naru/katalk/exception/DuplicateEmailException.java
+++
@@ -1,8 +0,0 @@
-package com.naru.katalk.exception;
-
-public class DuplicateEmailException extends RegisterException {
-
-    public DuplicateEmailException(final String message) {
-        super(message);
-    }
-}
--- src/main/java/com/naru/katalk/exception/RegisterException.java
+++
@@ -1,8 +0,0 @@
-package com.naru.katalk.exception;
-
-public class RegisterException extends RuntimeException {
-
-    public RegisterException(String message) {
-        super(message);
-    }
-}
