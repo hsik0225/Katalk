fd993bdec45fd4cbac85a787aeffaa0595b37839
naru <hsik0225@gmail.com>
2020-10-25 20:35:23 +0900

    feat : ErrorCode enum에 메세지를 포함한 해당 예외 처리 발생 이유도 갖고 있게 수정

--- src/main/java/com/naru/katalk/common/ErrorCode.java
+++ src/main/java/com/naru/katalk/common/ErrorCode.java
@@ -8,15 +8,18 @@ import lombok.Getter;
 public enum ErrorCode implements ResponseStatusCode {
 
     // Member
-    EMAIL_DUPLICATION("해당 이메일로 가입된 아이디가 존재합니다", HttpStatus.CONFLICT),
-    LOGIN_INPUT_INVALID("존재하지 않는 이메일이거나 비밀번호가 일치하지 않습니다", HttpStatus.UNAUTHORIZED);
-
-    private final String message;
+    EMAIL_DUPLICATION(HttpStatus.CONFLICT, "회원가입 실패", "해당 이메일로 가입된 아이디가 이미 존재합니다"),
+    LOGIN_INPUT_INVALID(HttpStatus.UNAUTHORIZED, "로그인 실패", "존재하지 않는 이메일이거나 비밀번호가 일치하지 않습니다");
 
     private final HttpStatus httpStatus;
 
-    ErrorCode(final String message, final HttpStatus httpStatus) {
-        this.message = message;
+    private final String message;
+
+    private final String reason;
+
+    ErrorCode(HttpStatus httpStatus, String message, String reason) {
         this.httpStatus = httpStatus;
+        this.message = message;
+        this.reason = reason;
     }
 }
