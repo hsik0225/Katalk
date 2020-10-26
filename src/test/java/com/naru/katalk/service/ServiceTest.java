4231eecc6f18606011d15b1c1b53a97d655135c0
naru <hsik0225@gmail.com>
2020-10-26 22:15:30 +0900

    refactor : ServiceTest에서 RestDocs 관련 어노테이션 삭제
    Service 단에선 RestDocs를 설정할 필요가 없으므로 삭제

--- src/test/java/com/naru/katalk/service/ServiceTest.java
+++ src/test/java/com/naru/katalk/service/ServiceTest.java
@@ -2,20 +2,8 @@ package com.naru.katalk.service;
 
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
-import org.springframework.restdocs.RestDocumentationExtension;
-import org.springframework.restdocs.mockmvc.RestDocumentationResultHandler;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-import com.naru.katalk.config.ResultHandlerConfiguration;
-
-@ExtendWith({RestDocumentationExtension.class, SpringExtension.class, MockitoExtension.class})
-@AutoConfigureRestDocs
-@ContextConfiguration(classes = ResultHandlerConfiguration.class)
+@ExtendWith({MockitoExtension.class})
 public abstract class ServiceTest {
 
-    @Autowired
-    private RestDocumentationResultHandler restDocumentation;
 }
