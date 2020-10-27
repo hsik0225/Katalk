33a606e6171a562a8d71320d2e1c3cfd24bc7c72
naru <hsik0225@gmail.com>
2020-10-27 21:05:46 +0900

    style : 로그인 실패 메세지 수정

--- src/main/java/com/naru/katalk/common/ErrorCode.java
+++ src/main/java/com/naru/katalk/common/ErrorCode.java
@@ -12,7 +12,7 @@ public enum ErrorCode implements ResponseStatusCode {
 
     // Member
     EMAIL_DUPLICATION(HttpStatus.CONFLICT, "회원가입 실패", "해당 이메일로 가입된 아이디가 이미 존재합니다"),
-    LOGIN_INPUT_INVALID(HttpStatus.UNAUTHORIZED, "로그인 실패", "존재하지 않는 이메일이거나 비밀번호가 일치하지 않습니다");
+    LOGIN_INPUT_INVALID(HttpStatus.UNAUTHORIZED, "존재하지 않는 이메일이거나 비밀번호가 일치하지 않습니다", null);
 
     private final HttpStatus httpStatus;
 
