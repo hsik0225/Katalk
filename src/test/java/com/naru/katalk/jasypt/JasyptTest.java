bc702774421764a6ca81f106b0b9df8628ae9e22
naru <hsik0225@gmail.com>
2020-10-28 18:58:32 +0900

    feat : jasypt 테스트 구현

---
+++ src/test/java/com/naru/katalk/jasypt/JasyptTest.java
@@ -0,0 +1,33 @@
+package com.naru.katalk.jasypt;
+
+import org.jasypt.encryption.StringEncryptor;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import com.naru.katalk.config.JasyptConfig;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@ExtendWith(SpringExtension.class)
+@Import(JasyptConfig.class)
+public class JasyptTest {
+
+    @Autowired
+    private StringEncryptor stringEncryptor;
+
+    @Test
+    public void initTest() {
+        assertThat(stringEncryptor).isNotNull();
+    }
+
+    @Test
+    public void jasyptTest() {
+        String encryptedEver = stringEncryptor.encrypt("test");
+        String decryptedEver = stringEncryptor.decrypt(encryptedEver);
+
+        assertThat(decryptedEver).isEqualTo("test");
+    }
+}
