748eab18e07f2dc34b8818cbb63c70813b29b4e7
naru <hsik0225@gmail.com>
2020-10-28 18:57:56 +0900

    refactor : Jasypt 비밀번호 환경변수로 설정 및 Jasypt 설정 변경

--- src/main/java/com/naru/katalk/config/JasyptConfig.java
+++ src/main/java/com/naru/katalk/config/JasyptConfig.java
@@ -9,17 +9,18 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class JasyptConfig {
 
-    private static final String password = System.getenv("JASYPT_PASSWORD");
+    private static final String PASSWORD = System.getenv("JASYPT_PASSWORD");
 
     @Bean("jasyptStringEncryptor")
     public StringEncryptor stringEncryptor() {
         SimpleStringPBEConfig config = new SimpleStringPBEConfig();
-        config.setPassword(password);
-        config.setAlgorithm("PBEWithMD5AndDES");
+        config.setPassword(PASSWORD);
+        config.setAlgorithm("PBEWITHHMACSHA512ANDAES_256");
         config.setKeyObtentionIterations("1000");
         config.setPoolSize("1");
         config.setProviderName("SunJCE");
         config.setSaltGeneratorClassName("org.jasypt.salt.RandomSaltGenerator");
+        config.setIvGeneratorClassName("org.jasypt.iv.RandomIvGenerator");
         config.setStringOutputType("base64");
 
         PooledPBEStringEncryptor encryptor = new PooledPBEStringEncryptor();
