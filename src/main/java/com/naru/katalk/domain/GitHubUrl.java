ba6c24d323439c69ace58f8d3ea8a136577dd632
naru <hsik0225@gmail.com>
2020-10-29 11:18:45 +0900

    feat : GitHub URL를 담고 있는 클래스 구현

---
+++ src/main/java/com/naru/katalk/domain/GitHubUrl.java
@@ -0,0 +1,10 @@
+package com.naru.katalk.domain;
+
+public class GitHubUrl {
+
+    public static final String CODE = "https://github.com/login/oauth/authorize";
+
+    public static final String USER_API = "https://api.github.com/user";
+
+    public static final String ACCESS_TOKEN = "https://github.com/login/oauth/access_token";
+}
